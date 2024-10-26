bfb478dd1f6165e3606ae38cd8de8bdd24102ab5
Ewan Belshaw <ewanbelshaw@gmail.com>
2024-10-26 00:57:19 -0400

    Finished the Stack ADT

--- a2/Stackadt.java
+++ a2/Stackadt.java
@@ -3,8 +3,11 @@ public class Stackadt {
 	
 	static int top = -1;
     static String stack[] = new String[2];
-
     
+
+    static boolean isEmpty() {
+    	boolean empty = top == -1 ? true : false; return empty;
+    }
     
     static boolean isFull(){
 
@@ -25,6 +28,8 @@ public class Stackadt {
 	static void push(String data) {
 		
 		if(isFull()) {
+			System.out.println("Stack is full; its current size is " + stack.length);
+			System.out.println("Will be expanding the size to " + stack.length*2);
 			String temp[] = new String[stack.length*2];
 			for(int i = 0;i < stack.length;i++) {
 				temp[i] = stack[i];
@@ -33,15 +38,36 @@ public class Stackadt {
 		}
 		
 		top++;
-		stack[top] = data; // help me
+		stack[top] = data; 
+		
 		
 	}
 	
+	static int size() {
+		return stack.length;
+	}
+	
+	static String top() {
+		if(top != -1) {
+			return stack[top];
+		}else {
+			return "too small!";
+		}
+	}
 	
     public static void main(String[] args) {
         
-    	
-    	
+    	push("hello");
+    	push("bonjours");
+    	push("grammarcy");
+    	System.out.println(top());
+    	System.out.println(top());
+    	pop();
+    	System.out.println(top());
+    	pop();
+    	System.out.println(top());
+    	pop();
+    	System.out.println(top());
     	
     	
     }
